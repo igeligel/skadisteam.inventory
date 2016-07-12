fc6881c6bc268f27e54f85607435bcca4a1fa927
igeligel <kevinigeligeligel@gmail.com>
2016-07-13 00:18:28 +0200

    refactor(skadiinventory)
    use methods and constants of the last commits

--- src/skadisteam.inventory/SkadiInventory.cs
+++ src/skadisteam.inventory/SkadiInventory.cs
@@ -2,56 +2,116 @@ using System;
 using skadisteam.inventory.Models;
 using System.Net;
 using System.Net.Http;
+using skadisteam.inventory.Constants;
+using skadisteam.inventory.Extensions;
+using skadisteam.inventory.Factories;
 
 namespace skadisteam.inventory
 {
     public class SkadiInventory
     {
-        public static void loadInventory(
+        public static void LoadInventory(
             SkadiLoadInventoryConfiguration skadiLoadInventory)
         {
-            var baseUrl = "http://steamcommunity.com";
-            var path = "/profiles/" +
-                      skadiLoadInventory.PartnerId +
-                      "/inventory/json/" + skadiLoadInventory.AppId + "/" +
-                      skadiLoadInventory.ContextId;
+            var path =
+                PathFactory.CreatePublicInventoryPath(
+                    skadiLoadInventory.PartnerId, skadiLoadInventory.AppId,
+                    skadiLoadInventory.ContextId);
+
             if (skadiLoadInventory.TradableItems)
             {
-                path += "?trading=1";
+                path = path.AddNewTradableItemParameter();
             }
 
-            Uri uri = new Uri(baseUrl);
-
-            CookieContainer _cookieContainer = new CookieContainer();
+            var cookieContainer = new CookieContainer();
             HttpResponseMessage response = null;
-            var handler = new HttpClientHandler();
-            handler.CookieContainer = _cookieContainer;
-            handler.AutomaticDecompression = DecompressionMethods.GZip |
-                                             DecompressionMethods.Deflate;
+            var handler = new HttpClientHandler
+            {
+                CookieContainer = cookieContainer,
+                AutomaticDecompression = DecompressionMethods.GZip |
+                                         DecompressionMethods.Deflate
+            };
             using (var client = new HttpClient(handler))
             {
-                
-                client.BaseAddress = uri;
+                client.BaseAddress = Uris.SteamCommunityBase;
                 client.DefaultRequestHeaders.TryAddWithoutValidation(
-                    "Cache-Control", "no-cache");
+                    HttpHeaderKeys.CacheControl, HttpHeaderValues.NoCache);
                 client.DefaultRequestHeaders.TryAddWithoutValidation(
-                    "Pragma", "no-cache");
+                    HttpHeaderKeys.Pragma, HttpHeaderValues.NoCache);
                 client.DefaultRequestHeaders.TryAddWithoutValidation(
-                    "Accept",
+                    HttpHeaderKeys.Accept,
                     "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
                 client.DefaultRequestHeaders.TryAddWithoutValidation(
-                    "Accept-Encoding", "gzip, deflate");
+                    HttpHeaderKeys.AcceptEncoding,
+                    HttpHeaderValues.GzipAndDeflate);
                 client.DefaultRequestHeaders.TryAddWithoutValidation(
-                    "Accept-Language",
-                    "de-DE,de;q=0.8,en-US;q=0.6,en;q=0.4,it;q=0.2");
+                    HttpHeaderKeys.AcceptLanguage,
+                    HttpHeaderValues.AcceptLanguage);
                 client.DefaultRequestHeaders.TryAddWithoutValidation(
-                    "User-Agent",
-                    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36");
+                    HttpHeaderKeys.UserAgent,
+                    HttpHeaderValues.UserAgent);
                 client.DefaultRequestHeaders.TryAddWithoutValidation(
-                    "Upgrade-Insecure-Requests", "1");
+                    HttpHeaderKeys.UpgradeInsecureRequests, "1");
                 
-                client.DefaultRequestHeaders.Host = uri.Host;
+                client.DefaultRequestHeaders.Host = Uris.SteamCommunityBase.Host;
 
+                response = client.GetAsync(path).Result;
+            }
+        }
+
+        public static void LoadPartnerInventory(long partnerCommunityId,
+            int appId, int contextId, string sessionId,
+            string steamLoginSecure, string tradeToken, bool tradableItems,
+            int steam32Id)
+        {
+            
+            var path = "/tradeoffer/new/partnerinventory/?sessionid=" +
+                       sessionId + "&partner=" + partnerCommunityId + "&appid=" +
+                       appId + "&contextid=" + contextId;
+            if (tradableItems)
+            {
+                path = path.AddTradableItemParameter();
+            }
+
+            var uri = new Uri(Resources.SteamCommunityBaseSecured);
+
+            var cookieContainer = new CookieContainer()
+                .AddEnglishSteamLanguageCookie()
+                .AddSteamSessionIdCookie(sessionId)
+                .AddSteamLoginSecureCookie(steamLoginSecure);
+
+            HttpResponseMessage response = null;
+            var handler = new HttpClientHandler
+            {
+                CookieContainer = cookieContainer,
+                AutomaticDecompression = DecompressionMethods.GZip |
+                                         DecompressionMethods.Deflate
+            };
+
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = uri;
+                client.DefaultRequestHeaders.TryAddWithoutValidation(
+                    HttpHeaderKeys.Accept,
+                    "text/javascript, text/html, application/xml, text/xml, */*");
+                client.DefaultRequestHeaders.TryAddWithoutValidation(
+                    HttpHeaderKeys.XPrototypeVersion, "1.7");
+                client.DefaultRequestHeaders.TryAddWithoutValidation(
+                    HttpHeaderKeys.XRequestedWith, "XMLHttpRequest");
+                client.DefaultRequestHeaders.TryAddWithoutValidation(
+                    HttpHeaderKeys.UserAgent,
+                    HttpHeaderValues.UserAgent);
+                client.DefaultRequestHeaders.TryAddWithoutValidation(
+                    HttpHeaderKeys.Referer,
+                    "https://steamcommunity.com/tradeoffer/new/?partner=" +
+                    steam32Id + "&token=" + tradeToken);
+                client.DefaultRequestHeaders.TryAddWithoutValidation(
+                    HttpHeaderKeys.AcceptEncoding,
+                    HttpHeaderValues.GzipDeflateSdhcAndBr);
+                client.DefaultRequestHeaders.TryAddWithoutValidation(
+                    HttpHeaderKeys.AcceptLanguage,
+                    HttpHeaderValues.AcceptLanguage);
+                client.DefaultRequestHeaders.Host = uri.Host;
                 response = client.GetAsync(path).Result;
             }
         }
