7d859e90e63037d3078547ced6526020be74791c
igeligel <kevinigeligeligel@gmail.com>
2016-07-13 00:17:50 +0200

    feat(factory)
    add factory for creating paths

---
+++ src/skadisteam.inventory/Factories/PathFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace skadisteam.inventory.Factories
+{
+    internal class PathFactory
+    {
+        internal static string CreatePublicInventoryPath(
+            long communityPartnerId, int appId, int contextId)
+        {
+            return "/profiles/" + communityPartnerId + "/inventory/json/" +
+                   appId + "/" + contextId;
+        }
+    }
+}
