b7ed67f6133e3566a65b73d8fea41c3a196417bf
igeligel <kevinigeligeligel@gmail.com>
2016-07-17 16:48:09 +0200

    refactor(models)
    change access modifiers

--- src/skadisteam.inventory/Models/Json/AppData.cs
+++ src/skadisteam.inventory/Models/Json/AppData.cs
@@ -5,12 +5,12 @@ namespace skadisteam.inventory.Models.Json
     public class AppData
     {
         [JsonProperty(PropertyName = "def_index")]
-        internal string DefIndex { get; set; }
+        public string DefIndex { get; set; }
 
         [JsonProperty(PropertyName = "is_itemset_name")]
-        internal int IsItemSetName { get; set; }
+        public int IsItemSetName { get; set; }
 
         [JsonProperty(PropertyName = "limited")]
-        internal int Limited { get; set; }
+        public int Limited { get; set; }
     }
 }
