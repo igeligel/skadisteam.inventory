5094729cebbd3cb303e60ac27644e32ecb56e2bc
igeligel <kevinigeligeligel@gmail.com>
2016-07-18 17:25:07 +0200

    docs(cookiecontainerextensions)
    add xml documentation for methods

--- src/skadisteam.inventory/Extensions/CookieContainerExtensions.cs
+++ src/skadisteam.inventory/Extensions/CookieContainerExtensions.cs
@@ -1,10 +1,24 @@
-﻿using System.Net;
+using System.Net;
 using skadisteam.inventory.Constants;
 
 namespace skadisteam.inventory.Extensions
 {
+    /// <summary>
+    /// Class which provide extensions for the CookieContainer class.
+    /// Lookup; <see cref="CookieContainer"/>.
+    /// </summary>
     internal static class CookieContainerExtensions
     {
+        /// <summary>
+        /// Add the steam language cookie to the <see cref="CookieContainer"/>,
+        /// given as parameter.
+        /// </summary>
+        /// <param name="cookieContainer">
+        /// <see cref="CookieContainer"/> which the cookie should be added to.
+        /// </param>
+        /// <returns>
+        /// The <see cref="CookieContainer"/> with appended steam language cookie.
+        /// </returns>
         internal static CookieContainer AddEnglishSteamLanguageCookie(
             this CookieContainer cookieContainer)
         {
@@ -15,6 +29,19 @@ namespace skadisteam.inventory.Extensions
             return cookieContainer;
         }
 
+        /// <summary>
+        /// Add the steam session cookie to the <see cref="CookieContainer"/>,
+        /// given as parameter.
+        /// </summary>
+        /// <param name="cookieContainer">
+        /// <see cref="CookieContainer"/> which the cookie should be added to.
+        /// </param>
+        /// <param name="sessionId">
+        /// Session Id which is grabbed at some steam site.
+        /// </param>
+        /// <returns>
+        /// The <see cref="CookieContainer"/> with appended steam session cookie.
+        /// </returns>
         internal static CookieContainer AddSteamSessionIdCookie(
             this CookieContainer cookieContainer, string sessionId)
         {
@@ -24,6 +51,20 @@ namespace skadisteam.inventory.Extensions
             return cookieContainer;
         }
 
+        /// <summary>
+        /// Add the steam login secure cookie to the <see cref="CookieContainer"/>,
+        /// given as parameter.
+        /// </summary>
+        /// <param name="cookieContainer">
+        /// <see cref="CookieContainer"/> which the cookie should be added to.
+        /// </param>
+        /// <param name="steamLoginSecure">
+        /// Steam Login Secure Cookie Value.
+        /// </param>
+        /// <returns>
+        /// The <see cref="CookieContainer"/> with 
+        /// the appended steam login secure cookie.
+        /// </returns>
         internal static CookieContainer AddSteamLoginSecureCookie(
             this CookieContainer cookieContainer, string steamLoginSecure)
         {
