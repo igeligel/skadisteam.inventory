fea8268078d1488761a3db82ca6bd4ac8576d6c5
igeligel <kevinigeligeligel@gmail.com>
2016-07-20 23:40:43 +0200

    docs(models)
    add xml documentation for all json models

--- src/skadisteam.inventory/Models/Json/Item.cs
+++ src/skadisteam.inventory/Models/Json/Item.cs
@@ -1,21 +1,41 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace skadisteam.inventory.Models.Json
 {
+    /// <summary>
+    /// Json Model for an item in the rgInventory object.
+    /// It represents just the bare item without any metainformation.
+    /// </summary>
     internal class Item
     {
+        /// <summary>
+        /// Asset Id of the item, which is used in several trading parts.
+        /// </summary>
         [JsonProperty(PropertyName = "id")]
         internal long Id { get; set; }
 
+        /// <summary>
+        /// Class id which is identifying the item.
+        /// </summary>
         [JsonProperty(PropertyName = "classid")]
         internal long ClassId { get; set; }
 
+        /// <summary>
+        /// Id to differentiate between items with the same class id.
+        /// </summary>
         [JsonProperty(PropertyName = "instanceid")]
         internal long InstanceId { get; set; }
 
+        /// <summary>
+        /// Amount of items. This is only used for special items
+        /// like gems of the Steam Community.
+        /// </summary>
         [JsonProperty(PropertyName = "amount")]
         internal int Amount { get; set; }
 
+        /// <summary>
+        /// Position of the item.
+        /// </summary>
         [JsonProperty(PropertyName = "pos")]
         internal int Pos { get; set; }
     }
--- src/skadisteam.inventory/Models/Json/Tag.cs
+++ src/skadisteam.inventory/Models/Json/Tag.cs
@@ -1,21 +1,41 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace skadisteam.inventory.Models.Json
 {
+    /// <summary>
+    /// Tags for items used. There are different types which
+    /// are differentiated by the category.
+    /// This tag contains additional information of the item.
+    /// </summary>
     internal class Tag
     {
+        /// <summary>
+        /// Internal name of the tag.
+        /// </summary>
         [JsonProperty(PropertyName = "internal_name")]
         internal string InternalName { get; set; }
 
+        /// <summary>
+        /// Name of the tag to show.
+        /// </summary>
         [JsonProperty(PropertyName = "name")]
         internal string Name { get; set; }
 
+        /// <summary>
+        /// Category of the tag.
+        /// </summary>
         [JsonProperty(PropertyName = "category")]
         internal string Category { get; set; }
 
+        /// <summary>
+        /// Name of the category.
+        /// </summary>
         [JsonProperty(PropertyName = "category_name")]
         internal string CategoryName { get; set; }
 
+        /// <summary>
+        /// Color of the category.
+        /// </summary>
         [JsonProperty(PropertyName = "color")]
         internal string Color { get; set; }
     }
