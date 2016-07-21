e82ecc5b204e850e13a1a51061e6e0a12240e57c
igeligel <kevinigeligeligel@gmail.com>
2016-07-21 17:21:14 +0200

    docs(client) and refactor(client)
    delete using and add xml documentation for methods

--- src/skadisteam.inventory/SkadiInventoryClient.cs
+++ src/skadisteam.inventory/SkadiInventoryClient.cs
@@ -1,4 +1,3 @@
-using System;
 using skadisteam.inventory.Models;
 using System.Net;
 using Newtonsoft.Json;
@@ -8,8 +7,31 @@ using skadisteam.inventory.Models.Json;
 
 namespace skadisteam.inventory
 {
-    public class SkadiInventoryClient
+    /// <summary>
+    /// Class which is used for requesting inventories. Just call the methods
+    /// without instancing this class.
+    /// </summary>
+    /// <example>
+    /// // Public inventory
+    /// var inventory = SkadiInventoryClient.LoadInventory(...);
+    /// 
+    /// // Partner Inventory
+    /// var privateInventory = 
+    ///     SkadiInventoryClient.LoadPartnerInventory(...);
+    /// </example>
+    public static class SkadiInventoryClient
     {
+        /// <summary>
+        /// Method to request the inventory of a public accessible inventory.
+        /// </summary>
+        /// <param name="skadiLoadInventory">
+        /// Configuration Instance of 
+        /// <see cref="SkadiLoadInventoryConfiguration"/>.
+        /// </param>
+        /// <returns>
+        /// An instance of <see cref="SkadiInventory"/>. Its a simplified
+        /// formatted data structure which holds the inventory.
+        /// </returns>
         public static SkadiInventory LoadInventory(
             SkadiLoadInventoryConfiguration skadiLoadInventory)
         {
@@ -28,7 +50,38 @@ namespace skadisteam.inventory
             return skadiInventory;
         }
 
-        public static void LoadPartnerInventory(long partnerCommunityId,
+        /// <summary>
+        /// Method to request the inventory of a private accessible inventory.
+        /// </summary>
+        /// <param name="partnerCommunityId">
+        /// Steam community id of the inventory requested.
+        /// </param>
+        /// <param name="appId">
+        /// App Id which should be requested.
+        /// </param>
+        /// <param name="contextId">
+        /// Context Id related to app id.
+        /// </param>
+        /// <param name="sessionId">
+        /// Session id of a login session.
+        /// </param>
+        /// <param name="steamLoginSecure">
+        /// Steam login secure value of a login session.
+        /// </param>
+        /// <param name="tradeToken">
+        /// Trade token of the profile which the inventory is requested for.
+        /// </param>
+        /// <param name="tradableItems">
+        /// Value which filters just for tradable items.
+        /// </param>
+        /// <param name="steam32Id">
+        /// Steam id in 32 Bit format.
+        /// </param>
+        /// <returns>
+        /// An instance of <see cref="SkadiInventory"/>. Its a simplified
+        /// formatted data structure which holds the inventory.
+        /// </returns>
+        public static SkadiInventory LoadPartnerInventory(long partnerCommunityId,
             int appId, int contextId, string sessionId,
             string steamLoginSecure, string tradeToken, bool tradableItems,
             int steam32Id)
@@ -46,6 +99,10 @@ namespace skadisteam.inventory
             var response = requestFactory.CreatePartnerInventory(path,
                 steam32Id, tradeToken);
             var responseBody = response.Content.ReadAsStringAsync().Result;
+            var inventory =
+                JsonConvert.DeserializeObject<RootInventory>(responseBody);
+            var skadiInventory = SkadiInventoryFactory.Create(inventory);
+            return skadiInventory;
         }
     }
 }
