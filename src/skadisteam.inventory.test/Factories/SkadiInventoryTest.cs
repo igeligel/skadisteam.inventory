6f1f156e7c3e4e49da6f48f777a3060a381bf090
igeligel <kevinigeligeligel@gmail.com>
2016-07-25 22:45:15 +0200

    test(factory)
    add tests for skadi inventory factory

---
+++ src/skadisteam.inventory.test/Factories/SkadiInventoryTest.cs
@@ -0,0 +1,51 @@
+using Newtonsoft.Json;
+using skadisteam.inventory.Factories;
+using skadisteam.inventory.Models.Json;
+using skadisteam.inventory.test.Constants;
+using Xunit;
+
+namespace skadisteam.inventory.test.Factories
+{
+    public class SkadiInventoryTest
+    {
+        [Fact]
+        public void CsGoInventoryCountOne()
+        {
+            var rootInventory = JsonConvert.DeserializeObject<RootInventory>(Inventories.CsGoOne);
+            var skadiInventory = SkadiInventoryFactory.Create(rootInventory);
+            Assert.Equal(4, skadiInventory.Items.Count);
+        }
+
+        [Fact]
+        public void CsGoInventoryCountTwo()
+        {
+            var rootInventory = JsonConvert.DeserializeObject<RootInventory>(Inventories.CsGoTwo);
+            var skadiInventory = SkadiInventoryFactory.Create(rootInventory);
+            Assert.Equal(753, skadiInventory.Items.Count);
+        }
+
+        [Fact]
+        public void CsGoInventoryCountThree()
+        {
+            var rootInventory = JsonConvert.DeserializeObject<RootInventory>(Inventories.CsGoThree);
+            var skadiInventory = SkadiInventoryFactory.Create(rootInventory);
+            Assert.Equal(24, skadiInventory.Items.Count);
+        }
+
+        [Fact]
+        public void SteamInventoryCountOne()
+        {
+            var rootInventory = JsonConvert.DeserializeObject<RootInventory>(Inventories.SteamOne);
+            var skadiInventory = SkadiInventoryFactory.Create(rootInventory);
+            Assert.Equal(58, skadiInventory.Items.Count);
+        }
+
+        [Fact]
+        public void TeamFortressInventoryCountOne()
+        {
+            var rootInventory = JsonConvert.DeserializeObject<RootInventory>(Inventories.TeamFortressOne);
+            var skadiInventory = SkadiInventoryFactory.Create(rootInventory);
+            Assert.Equal(85, skadiInventory.Items.Count);
+        }
+    }
+}
